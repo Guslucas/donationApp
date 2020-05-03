213c943d025e58cb60805671d7123389e87f729a
gustavo.rosa <gustavolucasdarosa@gmail.com>
2020-05-03 17:16:14 -0300

    Started Android Studio Project

---
+++ app/src/main/java/br/com/faj/project/donationapp/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.faj.project.donationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
