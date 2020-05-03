26019bf102524828061e2f6560a977a4ccf1c283
gustavo.rosa <gustavolucasdarosa@gmail.com>
2020-05-03 18:15:09 -0300

    Splash feito

--- app/src/main/java/br/com/faj/project/donationapp/MainActivity.java
+++ app/src/main/java/br/com/faj/project/donationapp/MainActivity.java
@@ -3,12 +3,20 @@ package br.com.faj.project.donationapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                setContentView(R.layout.activity_main);
+            }
+        }, 2000);
     }
 }
