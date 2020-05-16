50d9a6dba8398f27bfc91ca88e46637bd444d55f
gustavo.rosa <gustavolucasdarosa@gmail.com>
2020-05-16 14:21:49 -0300

    Adicionado Tela de Campanhas

---
+++ app/src/main/java/br/com/faj/project/donationapp/CampaignAdapter.java
@@ -0,0 +1,70 @@
+package br.com.faj.project.donationapp;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
+
+class CampaignAdapter extends RecyclerView.Adapter<CampaignAdapter.CampaignItemHolder> {
+
+    private List<Campaign> campaignList;
+    private Campaigns callingActivity;
+
+    CampaignAdapter(List<Campaign> campaignList, Campaigns callingActivity) {
+        this.campaignList = campaignList;
+        this.callingActivity = callingActivity;
+    }
+
+    @NonNull
+    @Override
+    public CampaignItemHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.campaign_item, parent, false);
+        return new CampaignItemHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CampaignItemHolder holder, int position) {
+        Campaign campaign = campaignList.get(position);
+        holder.mName.setText(campaign.getName());
+        holder.mDescription.setText(campaign.getDescription());
+        holder.mImage.setImageBitmap(campaign.getImage());
+        holder.mProgressBar.setProgress(campaign.getPercentage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return campaignList.size();
+    }
+
+    public class CampaignItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        ImageView mImage;
+        TextView mName;
+        TextView mDescription;
+        ProgressBar mProgressBar;
+
+        CampaignItemHolder(@NonNull View itemView) {
+            super(itemView);
+            mImage = itemView.findViewById(R.id.imageView);
+            mName = itemView.findViewById(R.id.nameTV);
+            mDescription = itemView.findViewById(R.id.descriptionTV);
+            mProgressBar = itemView.findViewById(R.id.progressBar);
+            itemView.setClickable(true);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            callingActivity.selectCampaign(getAdapterPosition());
+        }
+    }
+}
---
+++ app/src/main/java/br/com/faj/project/donationapp/Campaigns.java
@@ -0,0 +1,72 @@
+package br.com.faj.project.donationapp;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Campaigns extends AppCompatActivity {
+
+    List<Campaign> mCampaignList = new ArrayList<Campaign>();
+    RecyclerView mCampaignRecycler;
+    CampaignAdapter mCampaignAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.campaigns);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        loadCampaigns();
+
+        //getCampaigns
+        mCampaignRecycler = findViewById(R.id.campaignsRecycler);
+        mCampaignAdapter = new CampaignAdapter(mCampaignList, this);
+        RecyclerView.LayoutManager layoutManager =new LinearLayoutManager(getApplicationContext());
+        mCampaignRecycler.setLayoutManager(layoutManager);
+        mCampaignRecycler.setAdapter(mCampaignAdapter);
+        mCampaignRecycler.setItemAnimator(new DefaultItemAnimator());
+        mCampaignRecycler.addItemDecoration(new DividerItemDecoration(this, LinearLayout.VERTICAL));
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void loadCampaigns() {
+        Bitmap night = BitmapFactory.decodeResource(this.getResources(), R.drawable.night);
+        mCampaignList.add(new Campaign("Calm Nights", "Help us donation this.", night, 30));
+        mCampaignList.add(new Campaign("DARK Nights", "Help us donation this.", night, 30));
+    }
+
+    public void selectCampaign(int adapterPosition) {
+        Campaign c = mCampaignList.get(adapterPosition);
+        Toast.makeText(getApplicationContext(), "A campanha que você vai doar é " + c.getName(), Toast.LENGTH_SHORT).show();
+        //Intent i = new Intent(this);
+    }
+}
