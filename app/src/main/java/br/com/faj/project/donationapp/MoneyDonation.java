7c72e4f61dac9a28b65b8ab846bf5c2d604b3d78
Bruno.Haddad <brunolemoshaddad@gmail.com>
2020-05-16 16:52:18 -0300

    Tela MoneyDonation criada

---
+++ app/src/main/java/br/com/faj/project/donationapp/MoneyDonation.java
@@ -0,0 +1,14 @@
+package br.com.faj.project.donationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MoneyDonation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.money_donation);
+    }
+}
